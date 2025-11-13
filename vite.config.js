3d5eb63a28cc076c5a0f8ca7cd620276c9283609
LatrachDev <m.latrach.youcode@gmail.com>
2025-11-13 20:05:23 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,9 +11,10 @@ export default defineConfig({
       '@': path.resolve(process.cwd(), './src'),
     },
   },
+  base: process.env.GITHUB_PAGES === 'true' ? '/SC/' : '/',
   build: {
     target: 'esnext',
-    outDir: 'build',
+    outDir: 'dist',
   },
   server: {
     port: 3000,
